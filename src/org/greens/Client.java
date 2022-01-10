ed9368fa8b8ebd339bdd5820d4a6238952b4fd9a
gowtham24008 <gowtham24008@gmail.com>
2022-01-10 19:11:50 +0530

    task

--- src/org/greens/Client.java
+++ src/org/greens/Client.java
@@ -1,9 +1,8 @@
 package org.greens;
 
 public class Client {
-private void empDetails(String name) {
-	System.out.println("String method"+name);	
-}
+
+
 private void empDetails(String name , int id, long phoneno) {
 	System.out.println("count method"+name+"\n"+id);
 	System.out.println(phoneno);
@@ -19,7 +18,7 @@ private void empDetails(int pincode,String city) {
 }
 public static void main(String[] args) {
 	Client c = new Client();
-	c.empDetails("ganesh");
+	
 	c.empDetails("ganesh", 456745, 9761255869l);
 	c.empdetails(32.12455f);
 	c.empDetails("chenni", 607803);
