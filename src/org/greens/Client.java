1668e5ce0a521ded8089a3d6c36573c40a70810b
gowtham24008 <gowtham24008@gmail.com>
2022-01-10 19:24:16 +0530

    hang over

--- src/org/greens/Client.java
+++ src/org/greens/Client.java
@@ -1,7 +1,9 @@
 package org.greens;
 
 public class Client {
-
+private void empDetails(String name) {
+	System.out.println("String method"+name);
+}
 
 private void empDetails(String name , int id, long phoneno) {
 	System.out.println("count method"+name+"\n"+id);
@@ -16,13 +18,17 @@ private void empDetails(String city,int pincode) {
 private void empDetails(int pincode,String city) {
 	System.out.println("order method"+pincode+"\t"+city);
 }
+private void empdDetails(long phoneno,int id) {
+	System.out.println(phoneno+id);
+}
 public static void main(String[] args) {
 	Client c = new Client();
-	
+	c.empDetails("ganesh");
 	c.empDetails("ganesh", 456745, 9761255869l);
 	c.empdetails(32.12455f);
 	c.empDetails("chenni", 607803);
 	c.empDetails(456789, "covai");
+	c.empdDetails(9798455625l, 1234567);
 }
 }
 
